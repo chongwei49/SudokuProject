05744770c9988d878a063fbffcf4ff07bca18cc7
Sean Young <seanyoungsongjie@gmail.com>
2022-04-08 18:54:55 +0800

    Basic design and functionalities with hardcoded data

--- src/sudoku/GameBoard.java
+++ src/sudoku/GameBoard.java
@@ -1,46 +1,77 @@
 package sudoku;
+
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
 public class GameBoard extends JPanel {
    // Name-constants for the game board properties
-   public static final int GRID_SIZE = 9;    // Size of the board
+   public static final int GRID_SIZE = 9; // Size of the board
    public static final int SUBGRID_SIZE = 3; // Size of the sub-grid
+   public static final int GAP = 3; // Size of the sub-grid
+   private static final Color BG = Color.BLACK;
 
    // Name-constants for UI sizes
-   public static final int CELL_SIZE = 60;   // Cell width/height in pixels
-   public static final int BOARD_WIDTH  = CELL_SIZE * GRID_SIZE;
+   public static final int CELL_SIZE = 60; // Cell width/height in pixels
+   public static final int BOARD_WIDTH = CELL_SIZE * GRID_SIZE;
    public static final int BOARD_HEIGHT = CELL_SIZE * GRID_SIZE;
-                                             // Board width/height in pixels
+   // Board width/height in pixels
 
    // The game board composes of 9x9 "Customized" JTextFields,
    private Cell[][] cells = new Cell[GRID_SIZE][GRID_SIZE];
    // It also contains a Puzzle
-   private Puzzle puzzle = new Puzzle();
+   private Puzzle puzzle = Puzzle.getInstance();
 
    // Constructor
    public GameBoard() {
-      super.setLayout(new GridLayout(GRID_SIZE, GRID_SIZE));  // JPanel
+      JPanel mainPanel = new JPanel(new GridLayout(SUBGRID_SIZE, SUBGRID_SIZE));
+      // mainPanel.setLayout(new GridLayout(GRID_SIZE, GRID_SIZE)); // JPanel
+      mainPanel.setBorder(BorderFactory.createEmptyBorder(GAP, GAP, GAP, GAP));
+      mainPanel.setBackground(BG);
 
+      JPanel[][] panels = new JPanel[SUBGRID_SIZE][SUBGRID_SIZE];
       // Allocate the 2D array of Cell, and added into JPanel.
-      for (int row = 0; row < GRID_SIZE; ++row) {
-         for (int col = 0; col < GRID_SIZE; ++col) {
+      for (int row = 0; row < panels.length; ++row) {
+         for (int col = 0; col < panels[row].length; ++col) {
             cells[row][col] = new Cell(row, col);
-            super.add(cells[row][col]);   // JPanel
+            panels[row][col] = new JPanel(new GridLayout(SUBGRID_SIZE, SUBGRID_SIZE, 1, 1));
+            panels[row][col].setBackground(BG);
+            panels[row][col].setBorder(BorderFactory.createEmptyBorder(GAP, GAP, GAP, GAP));
+
+            mainPanel.add(panels[row][col]); // JPanel
          }
       }
 
+      for (int row = 0; row < cells.length; row++) {
+         for (int col = 0; col < cells[row].length; col++) {
+            cells[row][col] = new Cell(row, col);
+            int i = row / 3;
+            int j = col / 3;
+            panels[i][j].add(cells[row][col]);
+         }
+      }
+      super.setLayout(new BorderLayout());
+      super.add(mainPanel, BorderLayout.CENTER);
+
       // [TODO 3] Allocate a common listener as the ActionEvent listener for all the
-      //  Cells (JTextFields)
+      // Cells (JTextFields)
+      CellInputListener listener = new CellInputListener();
+
       // [TODO 4] Every editable cell adds this common listener
+      for (int row = 0; row < cells.length; row++) {
+         for (int col = 0; col < cells[row].length; col++) {
+            if (cells[row][col].isEditable()) {
+               cells[row][col].addActionListener(listener); // For all editable rows and cols
+            }
+         }
+      }
 
       super.setPreferredSize(new Dimension(BOARD_WIDTH, BOARD_HEIGHT));
    }
 
    /**
     * Initialize the puzzle number, status, background/foreground color,
-    *   of all the cells from puzzle[][] and isRevealed[][].
+    * of all the cells from puzzle[][] and isRevealed[][].
     * Call to start a new game.
     */
    public void init() {
@@ -71,5 +102,37 @@ public class GameBoard extends JPanel {
    }
 
    // [TODO 2] Define a Listener Inner Class
-   // ......
+   private class CellInputListener implements ActionListener {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+         // Get a reference of the JTextField that triggers this action event
+         Cell sourceCell = (Cell) e.getSource();
+
+         // Retrieve the int entered
+         int numberIn = Integer.parseInt(sourceCell.getText());
+         // For debugging
+         System.out.println("You entered " + numberIn);
+         System.out.println(sourceCell.number);
+
+         /*
+          * [TODO 5]
+          * Check the numberIn against sourceCell.number.
+          * Update the cell status sourceCell.status,
+          * and re-paint the cell via sourceCell.paint().
+          */
+         if (numberIn == sourceCell.number) {
+            sourceCell.status = CellStatus.CORRECT_GUESS;
+         } else {
+            sourceCell.status = CellStatus.WRONG_GUESS;
+         }
+         sourceCell.paint();
+
+         /*
+          * [TODO 6][Later] Check if the player has solved the puzzle after this move,
+          * by call isSolved(). Put up a congratulation JOptionPane, if so.
+          */
+         if (isSolved())
+            JOptionPane.showMessageDialog(null, "Congratulation!");
+      }
+   }
 }
--- src/sudoku/SudokuMain.java
+++ src/sudoku/SudokuMain.java
@@ -1,14 +1,20 @@
 package sudoku;
+
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.text.FlowView;
+
 /**
  * The main Sudoku program
  */
 public class SudokuMain extends JFrame {
    // private variables
    GameBoard board = new GameBoard();
-   JButton btnNewGame = new JButton("New Game");
+   JButton newGameBtn = new JButton("New Game");
+   // JPanel[][] paneles;
+   JPanel center, controllerPanel;
+   JButton restartGameBtn, cBtn, exitBtn, hardBtn, midBtn, easyBtn, slove, aboutBtn;
 
    // Constructor
    public SudokuMain() {
@@ -20,10 +26,71 @@ public class SudokuMain extends JFrame {
       // Add a button to the south to re-start the game
       // ......
 
-      board.init();
+      /*------------------------panel for buttons -------------------------------------*/
 
-      pack();     // Pack the UI components, instead of setSize()
-      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  // Handle window closing
+      controllerPanel = new JPanel();
+      controllerPanel.setBackground(Color.decode("#AABFFF"));
+      controllerPanel.setBorder(BorderFactory.createLineBorder(Color.black, 6, true));
+      controllerPanel.setLayout(new GridLayout(2, 3, 2, 20));
+
+      newGameBtn = new JButton("New Game");
+      newGameBtn.setSize(20, 50);
+      // timer = new Timer(1000, action);
+
+      /*------------------------panel for new game button -------------------------------------*/
+      newGameBtn.addActionListener(new ActionListener() {
+
+         @Override
+         public void actionPerformed(ActionEvent e) {
+            // counter = 0;
+            // timer.start();
+            // restgame();
+            // Sudoku.newGame();
+            board.init();
+         }
+      });
+
+      /*------------------------panal for new about button -------------------------------------*/
+      aboutBtn = new JButton("About");
+
+      aboutBtn.addActionListener(new ActionListener() {
+         @Override
+         public void actionPerformed(ActionEvent e) {
+            JOptionPane.showMessageDialog(cp, "By Sean Young and Luo Chong Wei");
+         }
+      });
+
+      /*------------------------panal for new New Game button -------------------------------------*/
+      restartGameBtn = new JButton("Restart Game");
+      restartGameBtn.addActionListener(new ActionListener() {
+
+         @Override
+         public void actionPerformed(ActionEvent e) {
+            board.init();
+         }
+      });
+
+      /*------------------------panal for new Exit button -------------------------------------*/
+      exitBtn = new JButton("Exit");
+      exitBtn.addActionListener(new ActionListener() {
+
+         @Override
+         public void actionPerformed(ActionEvent e) {
+            System.exit(0);
+         }
+      });
+
+      controllerPanel.add(newGameBtn);
+      controllerPanel.add(aboutBtn);
+      controllerPanel.add(restartGameBtn);
+      controllerPanel.add(exitBtn);
+
+      cp.add(controllerPanel, BorderLayout.SOUTH);
+
+      // board.init();
+
+      pack(); // Pack the UI components, instead of setSize()
+      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Handle window closing
       setTitle("Sudoku");
       setVisible(true);
    }
@@ -31,6 +98,12 @@ public class SudokuMain extends JFrame {
    /** The entry main() entry method */
    public static void main(String[] args) {
       // [TODO 1] Check Swing program template on how to run the constructor
-      //......
+      // ......
+      SwingUtilities.invokeLater(new Runnable() {
+         @Override
+         public void run() {
+            new SudokuMain(); // Let the constructor do the job
+         }
+      });
    }
 }
